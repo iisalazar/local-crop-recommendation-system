c5d008df87d18ace8832872853c1d1a10fe08cca
dertrockx <ivangrey67@gmail.com>
2018-09-05 14:42:07 +0000

    Added the script to read from the serial port of raspberry pi

---
+++ read_from_arduino.py
@@ -0,0 +1,18 @@
+import serial
+import RPi.GPIO as GPIO
+import time
+import sys
+
+ser = serial.Serial('/dev/ttyACM0', 9600)
+ser.baudrate=9600
+while True:
+	try:
+		read_ser = ser.readline()
+		print(str(read_ser))
+		time.sleep(1)
+	except KeyboardInterrupt:
+		print("\n Babye!!!")
+		sys.exit()
+	except:
+		print("Something fucked up!")
+
