c919b71dc6704b786ed6fb40a846610e52e567f2
dertrockx <ivangrey67@gmail.com>
2018-09-06 00:26:01 +0800

    Modified the arduino_script.py to be OOP

--- arduino_script.py
+++ arduino_script.py
@@ -1,46 +1,30 @@
 import serial
-import sqlite3
 import time
 import sys
+import sqlite3
 from datetime import datetime
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
-ser.baudrate=9600
+class Arduino:
+	def __init__(self):
+		self.ser = serial.Serial('/dev/ttyACM0', 9600)
+		self.ser.baudrate = 9600
 
-#class Arduino:
-#	def __init__(self, database):
-#		self.database = database
-#		self.ser = serial.Serial('/dev/ttyACM0', 9600)
-#		self.ser.baudrate = 9600
-# This function only writes on the database on every item
-# In the future, change this to write on the database in a single 'commit' not commit-ing every script
-#	def write_data(self, data):
-#		connection = sqlite3.connect(self.database)
-#		cursor = connection.cursor()
-#		cursor.executescript("INSERT INTO soil(date_measured, soil_moisture) VALUES('{}', {});".format(datetime.now(), data))
-#		connection.commit()
-#
-#	def read_data(self):
-#		try:
-#			read_ser = self.ser.readline()
-#			print(str(read_ser))
-#			return read_ser
-#			time.sleep(1)
-#		except KeyboardInterrupt:
-#			print('\n Bye!')
-#			sys.exit()
-#		except:
-#			print("Something fucked up!")
-#for d in range(5):
-#	write_data(d)
+	def read_data(self):
+		try:
+			read_ser = self.ser.readline()
+			print(str(read_ser))
+			return read_ser
+		except:
+			print("Something went wrong")
 
-#if __name__ == '__main__':
-#	app = Arduino('database.db')
-#	a = app.read_data()
-while True:
-	read = ser.readline()
-	print(read)
-	time.sleep(1)
-#		ard_data = app.read_data()
-#		app.write_data(ard_data)
-#		print(ard_data)
+	def write_data(self, data):
+		connection = sqlite3.connect('database.db')
+		cursor = connection.cursor()
+		cursor.executescript("INSERT INTO soil(date_measured, soil_moisture) VALUES('{}', {});".format(datetime.now(), data))
+		connection.commit()
+
+if __name__ == '__main__':
+	arduino = Arduino()
+	data = arduino.read_data()
+	arduino.write_data(data)
+#	print(a)
